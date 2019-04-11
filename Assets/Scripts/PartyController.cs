27c1eec711686423f9005dacad64b898049caf4f
bubblebooy <bubblebooy@gmail.com>
2019-04-11 10:12:09 -0500

    Added very basic movement script

---
+++ Assets/Scripts/PartyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyController : MonoBehaviour
+{
+    public float Gold;
+    public List<GameObject> Quests;
+    public List<MonoBehaviour> CurrentBehaviours;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Travel travel = gameObject.AddComponent<Travel>();
+        travel.targetLocation = Quests[0];
+        CurrentBehaviours.Add(travel);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentBehaviours.RemoveAll(behaviour => behaviour == null);
+        // if CurrentBehaviours = 0
+        //      if quest finnish quest
+        //      if at inn get new quest
+        //      else travle to inn
+    }
+}
---
+++ Assets/Scripts/Travel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Travel : MonoBehaviour
+{
+    public GameObject targetLocation;
+    public float speed = 1;
+    private float step;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = 2 * speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetLocation.transform.position, step);
+        if (Vector3.Distance(targetLocation.transform.position, transform.position) < 0.05f)
+        {
+            Destroy(this);
+        }
+    }
+}
